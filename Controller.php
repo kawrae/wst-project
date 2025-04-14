a01d52fc36d7202e60b3451d3e074999336ee4cc
Corey Black <coreyblack@hotmail.co.uk>
2025-04-14 22:20:59 +0100

    Fixed duplicate image upload

--- Controller.php
+++ Controller.php
@@ -6,12 +6,11 @@ $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
 if (!isset($token['error'])) {
 
     $google_client->setAccessToken($token['access_token']);
-
     $google_service = new Google_Service_Oauth2($google_client);
-
     $data = $google_service->userinfo->get();
 
     $name = $email = $image = '';
+    $image_url = $data['picture'] ?? '';
 
     if (!empty($data['given_name'])) {
         $name = $data['given_name'];
@@ -21,14 +20,6 @@ if (!isset($token['error'])) {
         $email = $data['email'];
     }
 
-    if (!empty($data['picture'])) {
-        $image_url = $data['picture'];
-        $image_name = uniqid() . '.jpg';
-        file_put_contents('uploaded_img/' . $image_name, file_get_contents($image_url));
-        $image = $image_name;
-    }
-
-    // check if the user already exists in the database
     $check = $conn->prepare("SELECT * FROM user_form WHERE email = ?");
     $check->bind_param("s", $email);
     $check->execute();
@@ -38,6 +29,12 @@ if (!isset($token['error'])) {
         $row = $result->fetch_assoc();
         $user_id = $row['id'];
     } else {
+        if (!empty($image_url)) {
+            $image_name = uniqid() . '.jpg';
+            file_put_contents('uploaded_img/' . $image_name, file_get_contents($image_url));
+            $image = $image_name;
+        }
+
         $stmt = $conn->prepare("INSERT INTO user_form (name, email, image) VALUES (?, ?, ?)");
         $stmt->bind_param("sss", $name, $email, $image);
         $stmt->execute();
@@ -47,7 +44,6 @@ if (!isset($token['error'])) {
 
     $check->close();
 
-    // store user ID in session
     session_start();
     $_SESSION['user_id'] = $user_id;
 
@@ -55,7 +51,5 @@ if (!isset($token['error'])) {
     exit;
 
 } else {
-    // Handle error
     echo "Error: " . $token['error'];
 }
-?>
